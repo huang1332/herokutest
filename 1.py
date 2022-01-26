ebe927599a702209cfa934a76bca42e0314aa84b
huang1332 <38836674+huang1332@users.noreply.github.com>
2022-01-26 18:08:21 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,2 +1,15 @@
-import time
-print(time.time())
+import streamlit as st
+st.set_page_config(
+     page_title='test',
+     layout="wide",
+     page_icon='🅿',#random
+     initial_sidebar_state="collapsed"#“auto”或“expanded”或“collapsed”
+)
+if 'str1' not in st.session_state:
+    st.session_state.str1 = ''
+title = st.text_area('test',height=500)
+def increment_counter(title):
+    exec(title)
+increment = st.button('exec', on_click=increment_counter,
+    args=(title, ))
+
